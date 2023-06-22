8c68d0e0efc011bf2a6bb01e871d648ab29cc9f9
Mubashir <mubashir15700@gmail.com>
2023-06-22 13:29:45 +0530

    fix validation

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,16 +3,16 @@
 $("#submit-form").submit((e) => {
     e.preventDefault();
     let name = document.querySelector('.contact__name').value;
-    let mail = document.querySelector('.contact__mail').value;
-    let message = document.querySelector('.contact__message').value;
+    let email = document.querySelector('.contact__mail').value;
     const patternName = /[\w\s\][,][0-9]/gi;
-    if (name && mail) {
-        if(name.length <= 3) {
+    if (name && email) {
+        if (name.length <= 3) {
             document.querySelector("#err-msg").innerHTML = "⚠️ Name must contain morethan 3 characters.";
-        } else if(patternName.test(name)) {
-            document.querySelector("#err-msg").innerHTML = "⚠️ Name should not contain numbers.";
+        } else if (patternName.test(name)) {
+            document.querySelector("#err-msg").innerHTML = "⚠️ Name should contain letters only.";
         } else {
-            if (mail.includes("@" && ".")) {
+            let patternEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if(patternEmail.test(email)) {
                 $.ajax({
                     url: "https://script.google.com/macros/s/AKfycbyIftpIrEJ8U0h861SL37VUmfK852neS2LEQY1P3hVdiT5tjr6VcZgr6hsPcWSKUeuR_Q/exec",
                     data: $("#submit-form").serialize(),
@@ -20,17 +20,16 @@ $("#submit-form").submit((e) => {
                     success: function (response) {
                         alert("Form submitted successfully");
                         window.location.reload();
-                        //window.location.href="https://google.com"
                     },
                     error: function (err) {
                         alert("Something Error");
-                    }   
+                    }
                 });
             } else {
-                document.querySelector("#err-msg").innerHTML = "⚠️ Please enter a valid email!";
+                document.querySelector("#err-msg").innerHTML = "⚠️ Email is invalid.";
             }
         }
     } else {
-        document.querySelector("#err-msg").innerHTML = "⚠️ Name and Email are required!";
+        document.querySelector("#err-msg").innerHTML = "⚠️ Name and Email are required.";
     }
 });
