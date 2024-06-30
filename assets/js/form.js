54a6adbcc03ecbf37d7afbefea21e9584d2b01b3
Mubashir15700 <mubashir15700@gmail.com>
2024-06-30 12:24:51 +0530

    update: add new skills, sent contact to email

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,32 +1,32 @@
-/*==================== FORM VALIDATION ====================*/
-
 $("#submit-form").submit((e) => {
     e.preventDefault();
+    let form = document.querySelector('#submit-form');
     let name = document.querySelector('.contact__name').value;
     let email = document.querySelector('.contact__mail').value;
-    const patternName = /[\w\s\][,][0-9]/gi;
+    const patternName = /^[a-zA-Z\s]+$/; // Only letters and spaces
+
     if (name && email) {
         if (name.length <= 3) {
-            document.querySelector("#err-msg").innerHTML = "⚠️ Name must contain morethan 3 characters.";
-        } else if (patternName.test(name)) {
+            document.querySelector("#err-msg").innerHTML = "⚠️ Name must contain more than 3 characters.";
+        } else if (!patternName.test(name)) {
             document.querySelector("#err-msg").innerHTML = "⚠️ Name should contain letters only.";
         } else {
-            let patternEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if(patternEmail.test(email)) {
-                $.ajax({
-                    url: "https://script.google.com/macros/s/AKfycbyIftpIrEJ8U0h861SL37VUmfK852neS2LEQY1P3hVdiT5tjr6VcZgr6hsPcWSKUeuR_Q/exec",
-                    data: $("#submit-form").serialize(),
-                    method: "post",
-                    success: function (response) {
-                        alert("Form submitted successfully");
-                        window.location.reload();
-                    },
-                    error: function (err) {
-                        alert("Something Error");
-                    }
+            let patternEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (patternEmail.test(email)) {
+                form.addEventListener("submit", function (event) {
+                    event.preventDefault();
+                    // these IDs from the previous steps
+                    emailjs.sendForm('service_m9rjy85', 'template_v6y7ahp', this)
+                        .then(() => {
+                            alert("Submitted");
+                            console.log('SUCCESS!');
+                        }, (error) => {
+                            alert("Failed to submit: ", error);
+                            console.log('FAILED...', error);
+                        });
                 });
             } else {
-                document.querySelector("#err-msg").innerHTML = "⚠️ Email is invalid.";
+                document.querySelector("#err-msg").innerHTML = "⚠️ Please enter a valid email.";
             }
         }
     } else {
